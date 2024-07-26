5e6e2d2c3bcc131777e9926d3ade0ff0534248c4
iseenoev1l <74930410+ic3y-csg0@users.noreply.github.com>
2024-07-26 23:05:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const indexRouter = require('./routes/index');
-const port = 3000;
+
 // Настройка EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-
 
 // Настройка статических файлов
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,13 +13,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Подключение маршрутизатора
 app.use('/', indexRouter);
 
-// Запуск сервера
-const PORT = process.env.PORT || 3000;
-app.listen(3000, '0.0.0.0', () => {
-    console.log('Server is running on http://0.0.0.0:3000');
-});
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
+module.exports = app;
