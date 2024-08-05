4e0c26b09a4355363398daafd638ad87ec8b1c0d
iseenoev1l <74930410+ic3y-csg0@users.noreply.github.com>
2024-08-05 22:17:28 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,9 @@ router.get('/menu', (req, res) => {
     res.render('menu');
 });
 router.get('/checkout', (req, res) => {
-    res.render('checkout');
+    res.render('checkout', { title: 'Checkout' });
+});
+router.get('/admin', (req, res) => {
+    res.render('admin', { title: 'Admin' });
 });
 module.exports = router;
